6ab79e2820f5f9d4ba54605aaa3240b63027cf4c
Lucas Quiej <quiej7959@gmail.com>
2025-05-12 12:58:12 -0600

    Mejoras en los archivos

--- index.js
+++ index.js
@@ -2,26 +2,30 @@ let bottones = document.querySelector('#boton-')
 let ul = document.querySelector('ul')
 let entrada = document.querySelector('#lista')
 // creo li   
+let tareas = []
 
 
 bottones.addEventListener('click', () => {
-    if(String(entrada.value).trim()=="")
-    { 
-         entrada.value=''
-         entrada.placeholder='Archivo vacio'
-    }else{
+    if (String(entrada.value).trim() == "") {
+        entrada.value = ''
+        entrada.placeholder = 'No se puede guardar un archivo sin nombre'
+    } else {
         agregar()
-        entrada.placeholder='What Will You Do'
+        entrada.placeholder = 'Add a new task'
     }
 })
 const agregar = () => {
+    let nuevatarea = {
+        texto: entrada.value,
+        isCompleted: false,
+    }
     let li = document.createElement('li')
     let div = document.createElement('div')
     div.classList.add('from-control')
     let input = document.createElement('input')
     input.setAttribute('readOnly', true)
     input.classList = 'tarea'
-    input.value = entrada.value
+    input.value = nuevatarea.texto
     entrada.value = ""
     let boton1 = document.createElement('button')
     boton1.classList = 'red'
@@ -35,26 +39,32 @@ const agregar = () => {
     div.appendChild(input)
     div.appendChild(boton1)
     div.appendChild(boton2)
-    console.log(ul)
-    boton1.addEventListener('click', () => {
+
+    boton1.addEventListener('click', (event) => {
         ul.removeChild(li)
+        tareas = tareas.filter(item => item.texto != event.target.previousElementSibling.value)
+        console.log(tareas)
     })
     boton2.addEventListener('click', () => {
         input.classList = 'lineamedia'
+        boton2.remove()
     })
+    tareas.push(nuevatarea)
+    console.log(tareas)
 }
 
 entrada.addEventListener('keydown', (event) => {
     if (event.key == 'Enter') {
-        if(String(entrada.value).trim()==''){
-            entrada.value=''
-            entrada.placeholder='Archivo vacio'
+        if (String(entrada.value).trim() == '') {
+            entrada.value = ''
+            entrada.placeholder = 'No se puede guardar un archivo sin nombre'
 
-        }else{
-            entrada.placeholder='What Will You Do'
-        agregar()}
+        } else {
+            entrada.placeholder = 'Add a new task'
+            agregar()
+        }
     }
 
- 
+
 })
 
